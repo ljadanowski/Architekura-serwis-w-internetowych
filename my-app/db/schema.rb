6d35ae3b80f4acfc94a11b2d3d99736668528650
Lukasz Jadanowski <lukasz.jadanowski@gmail.com>
2015-04-13 15:26:25 +0200

    naprawa aplikacji tak aby dzialala na produkcji

--- my-app/db/schema.rb
+++ my-app/db/schema.rb
@@ -11,7 +11,97 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317111440) do
+ActiveRecord::Schema.define(version: 20150411180346) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title",            limit: 50, default: ""
+    t.text     "comment"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.integer  "user_id"
+    t.string   "role",                        default: "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
+  add_index "comments", ["commentable_type"], name: "index_comments_on_commentable_type", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.integer  "language_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "courses", ["language_id"], name: "index_courses_on_language_id", using: :btree
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",               default: 0, null: false
+    t.integer  "attempts",               default: 0, null: false
+    t.text     "handler",                            null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by",  limit: 255
+    t.string   "queue",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "exercises", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "user_id"
+    t.integer  "level_id"
+    t.boolean  "visible",    default: false
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string   "lang"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "exercise_id"
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.integer  "language_id"
+    t.integer  "comment_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "lessons", ["comment_id"], name: "index_lessons_on_comment_id", using: :btree
+  add_index "lessons", ["language_id"], name: "index_lessons_on_language_id", using: :btree
+
+  create_table "levels", force: :cascade do |t|
+    t.string   "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "manifests", force: :cascade do |t|
+    t.integer  "exercise_id"
+    t.integer  "level_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "price"
@@ -21,7 +111,7 @@ ActiveRecord::Schema.define(version: 20150317111440) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "orders", ["person_id"], name: "index_orders_on_person_id"
+  add_index "orders", ["person_id"], name: "index_orders_on_person_id", using: :btree
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
@@ -33,4 +123,67 @@ ActiveRecord::Schema.define(version: 20150317111440) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "rankings", force: :cascade do |t|
+    t.integer  "point"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "solutions", force: :cascade do |t|
+    t.integer  "exercise_id", null: false
+    t.string   "code"
+    t.integer  "user_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "language_id"
+    t.string   "result"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "nickname"
+    t.string   "email",                                  null: false
+    t.string   "encrypted_password",                     null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "www"
+    t.string   "organization"
+    t.boolean  "job",                    default: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.boolean  "admin",                  default: false
+  end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "validations", force: :cascade do |t|
+    t.text     "correct"
+    t.integer  "exercise_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.string   "input"
+  end
+
+  add_foreign_key "courses", "languages"
+  add_foreign_key "lessons", "comments"
+  add_foreign_key "lessons", "languages"
 end
